16a08f014b2452ea56d64ca25806d134e03db33d
saurabh <saury.shah@gmail.com>
2019-09-14 11:06:38 +0530

    adding css and grid ui

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <CardList monsters = {this.state.monsters}>
+        <CardList monsters={this.state.monsters}>
         </CardList>
 
       </div >
