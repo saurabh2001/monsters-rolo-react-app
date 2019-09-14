cc06257c57cf9c465ba30e0bc82bb4d8cc7a8015
saurabh <saury.shah@gmail.com>
2019-09-14 07:53:27 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'saurabh'
+    };
+
+  }
+alertMessage(){
+  console.log(this.state);
 }
+
+  render() {
+    return (
+
+      <div className='App'>
+        <img src={logo} className='App-logo' alt='alternet text for'></img>
+        <p>{this.state.name}</p>
+        <button onClick={() => this.setState({name:'sauu'})}> Update the state</button> 
+        <br></br>
+        <button onClick={() => this.alertMessage()}> Print to console</button> 
+      </div >
+    )
+  }
+}
+
+
 
 export default App;
